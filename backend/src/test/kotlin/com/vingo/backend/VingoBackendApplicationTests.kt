0cc4bcb5251f9aaa07123f2e7d20525a3dfa3b79
Child-1 <thrillgods@gmail.com>
2025-11-08 19:16:47 +0100

    feat: hello API + Docker setup

---
+++ backend/src/test/kotlin/com/vingo/backend/VingoBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vingo.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VingoBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
